769e96854d35e7c6936db0f8ab37008f9bd85702
Radeonxz <xuanxxzhao@gmail.com>
2022-07-31 23:10:43 -0400

    fix: drag board position

--- client/src/apis/boardApi.ts
+++ client/src/apis/boardApi.ts
@@ -2,7 +2,8 @@ import httpClient from "./httpClient";
 
 const boardApi = {
   create: () => httpClient.post("boards"),
-  getAll: () => httpClient.get("boards")
+  getAll: () => httpClient.get("boards"),
+  updatePosition: (params: any) => httpClient.put("boards", params)
 };
 
 export default boardApi;
--- client/src/components/common/Sidebar.tsx
+++ client/src/components/common/Sidebar.tsx
@@ -57,7 +57,21 @@ const Sidebar = () => {
     navigate("/login");
   };
 
-  const onDragEnd = () => {};
+  const onDragEnd = async ({ source, destination }: any) => {
+    const newList = [...boards];
+    const [removed] = newList.splice(source.index, 1);
+    newList.splice(destination.index, 0, removed);
+
+    const activeItem = newList.findIndex((e) => e.id === boardId);
+    setActiveIndex(activeItem);
+    dispatch(setBoards(newList));
+
+    try {
+      await boardApi.updatePosition({ boards: newList });
+    } catch (err) {
+      alert(err);
+    }
+  };
 
   return (
     <Drawer
@@ -175,6 +189,7 @@ const Sidebar = () => {
                       )}
                     </Draggable>
                   ))}
+                  {provided.placeholder}
                 </div>
               )}
             </Droppable>
