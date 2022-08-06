7badb474fa17d0cb06e53e86f4070f1232237a2a
Radeonxz <xuanxxzhao@gmail.com>
2022-08-05 23:48:13 -0400

    feat: add emoji picker

--- client/src/apis/boardApi.ts
+++ client/src/apis/boardApi.ts
@@ -5,7 +5,9 @@ const boardApi = {
   getAll: () => httpClient.get("boards"),
   updatePosition: (params: any) => httpClient.put("boards", params),
   getBoardById: (boardId: string | undefined) =>
-    httpClient.get(`boards/${boardId}`)
+    httpClient.get(`boards/${boardId}`),
+  update: (boardId: string | undefined, params: any) =>
+    httpClient.put(`boards/${boardId}`, params)
 };
 
 export default boardApi;
--- client/src/pages/Board.tsx
+++ client/src/pages/Board.tsx
@@ -1,13 +1,27 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Box, IconButton } from "@mui/material";
-import StartOutlinedIcon from "@mui/icons-material/StartOutlined";
-import StartBorderOutlinedIcon from "@mui/icons-material/StartBorderOutlined";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Box,
+  IconButton,
+  Button,
+  TextField,
+  Typography,
+  Divider
+} from "@mui/material";
+import StarOutlinedIcon from "@mui/icons-material/StarOutlined";
+import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 
 import boardApi from "../apis/boardApi";
+import { setBoards } from "../redux/features/boardSlice";
+import EmojiPicker from "../components/common/EmojiPicker";
 
 const Board = () => {
+  const dispatch = useDispatch();
   const { boardId } = useParams();
+  const boards = useSelector((state: any) => state.board.value);
+
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [sections, setSections] = useState([]);
@@ -30,6 +44,19 @@ const Board = () => {
     getBoard();
   }, [boardId]);
 
+  const onIconChange = async (newIcon: string) => {
+    let temp = [...boards];
+    const index = temp.findIndex((e) => e.id === boardId);
+    temp[index] = { ...temp[index], icon: newIcon };
+    setIcon(newIcon);
+    dispatch(setBoards(temp));
+    try {
+      await boardApi.update(boardId, { icon: newIcon });
+    } catch (err) {
+      alert(err);
+    }
+  };
+
   return (
     <>
       <Box
@@ -40,13 +67,66 @@ const Board = () => {
           width: "100%"
         }}
       >
-        <IconButton variant="outlined">
+        <IconButton>
           {isFavorite ? (
-            <StartOutlinedIcon color="warning" />
+            <StarOutlinedIcon color="warning" />
           ) : (
-            <StartBorderOutlinedIcon />
+            <StarBorderOutlinedIcon />
           )}
         </IconButton>
+        <IconButton color="error">
+          <DeleteOutlinedIcon />
+        </IconButton>
+      </Box>
+      <Box>
+        <Box sx={{ padding: "10px 50px" }}>
+          <Box>
+            <EmojiPicker icon={icon} onChange={onIconChange} />
+            <TextField
+              value={title}
+              placeholder="Untitled"
+              variant="outlined"
+              fullWidth
+              sx={{
+                "& .MuiOutlinedInput-input": { padding: 0 },
+                "& .MuiOutlinedInput-notchedOutline": { border: "unset" },
+                "& .MuiOutlinedInput-root": {
+                  fontSize: "2rem",
+                  fontWeight: "700"
+                }
+              }}
+            />
+            <TextField
+              value={description}
+              placeholder="Add a description"
+              variant="outlined"
+              multiline
+              fullWidth
+              sx={{
+                "& .MuiOutlinedInput-input": { padding: 0 },
+                "& .MuiOutlinedInput-notchedOutline": { border: "unset" },
+                "& .MuiOutlinedInput-root": {
+                  fontSize: "0.8rem"
+                }
+              }}
+            />
+          </Box>
+        </Box>
+        <Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignments: "center",
+              justifyContent: "space-between"
+            }}
+          >
+            <Button>Add a section</Button>
+            <Typography variant="body2" fontWeight="700">
+              {sections.length} Sections
+            </Typography>
+          </Box>
+          <Divider sx={{ margin: "10px" }} />
+        </Box>
       </Box>
     </>
   );
