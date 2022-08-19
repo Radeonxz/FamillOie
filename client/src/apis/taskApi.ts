0e93ae2ac4a6a8ff08e0240a9f10d82586a8d160
RadeonXz <xuanxxzhao@gmail.com>
2022-08-18 22:17:21 -0400

    feat: add tasks

---
+++ client/src/apis/taskApi.ts
@@ -0,0 +1,16 @@
+import httpClient from './httpClient';
+
+const taskApi = {
+  create: (boardId: string, params: any) => httpClient.post(`boards/${boardId}/tasks`, params),
+  updatePosition: (boardId: string, params: any) => httpClient.put(
+    `boards/${boardId}/tasks/update-position`,
+    params
+  ),
+  delete: (boardId: string, taskId: string) => httpClient.delete(`boards/${boardId}/tasks/${taskId}`),
+  update: (boardId: string, taskId: string, params: any) => httpClient.put(
+    `boards/${boardId}/tasks/${taskId}`,
+    params
+  )
+};
+
+export default taskApi;
--- client/src/components/common/Section.tsx
+++ client/src/components/common/Section.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import {
   Box,
   Button,
+  Card,
   Typography,
   Divider,
   TextField,
@@ -12,6 +13,10 @@ import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 import sectionApi from "../../apis/sectionApi";
+import taskApi from "../../apis/taskApi";
+
+let timer: any;
+const timeout = 500;
 
 const Section = ({ boardId, data }: any) => {
   const [newData, setNewData] = useState<any>([]);
@@ -38,6 +43,34 @@ const Section = ({ boardId, data }: any) => {
       alert(err);
     }
   };
+
+  const updateSectionTitle = async (e: any, sectionId: string) => {
+    clearTimeout(timer);
+    const newTitle = e.target.value;
+    const newSections = [...newData];
+    const index = newSections.findIndex(e => e.id === sectionId);
+    newSections[index].title = newTitle;
+    setNewData(newSections);
+    timer = setTimeout(async() => {
+      try {
+        await sectionApi.update(boardId, sectionId, { title: newTitle });
+      } catch (err) {
+        alert(err);
+      }
+    }, timeout);
+  };
+
+  const createTask = async (sectionId: string) => {
+    try {
+      const task = await taskApi.create(boardId, { sectionId });
+      const newSections = [...newData];
+      const index = newSections.findIndex((e: any) => e.id === sectionId);
+      newSections[index].tasks.unshift(task);
+      setNewData(newSections);
+    } catch (err) {
+      alert(err);
+    }
+  }
 
   const onDragEnd = () => {};
 
@@ -87,7 +120,8 @@ const Section = ({ boardId, data }: any) => {
                       }}
                     >
                       <TextField
-                        value={section.id}
+                        value={section.title}
+                        onChange={(e) => updateSectionTitle(e, section.id)}
                         placeholder="Untitled"
                         variant="outlined"
                         sx={{
@@ -106,6 +140,7 @@ const Section = ({ boardId, data }: any) => {
                       <IconButton
                         size="small"
                         sx={{ color: "grey", "&:hover": { color: "green" } }}
+                        onClick={() => createTask(section.id)}
                       >
                         <AddOutlinedIcon />
                       </IconButton>
@@ -118,6 +153,20 @@ const Section = ({ boardId, data }: any) => {
                         <DeleteOutlinedIcon />
                       </IconButton>
                     </Box>
+                    {
+                      section.tasks.map((task: any, index: number) =>(
+                        <Draggable key={task.id} draggableId={task.id} index={index}>
+                          {(provided, snapshot) => (
+                            <Card ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} sx={{padding: '10px', marginBottom: '10px', cursor: snapshot.isDragging ? 'grab' : 'pointer!important'}}>
+                            <Typography>
+                              {task.title === '' ? 'Untitled' : task.title}
+                            </Typography>
+                          </Card>
+                          )}
+                        </Draggable>
+                      ))
+                    }
+                    {provided.placeholder}
                   </Box>
                 )}
               </Droppable>
